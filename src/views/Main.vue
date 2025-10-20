d8832a83868a4bd900cf4556aca68e46d228c6de
Ahmedeweis <93427804+Ahmedeweis@users.noreply.github.com>
2025-10-20 17:38:08 +0300

    send porftolio

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -671,9 +671,9 @@
 </a>
 <!-- Portfolio Box -->
 <a href="https://falcon-core.vercel.app/" target="_blank"
-  class="fixed bottom-4 left-28 flex items-center gap-2 bg-[#313847] text-white px-4 py-2 rounded-lg shadow-lg hover:bg-[#3e4652] transition-all duration-300">
+  class="fixed bottom-4 left-33 flex items-center gap-2 bg-[#313847] text-white px-4 py-2 rounded-lg shadow-lg hover:bg-[#3e4652] transition-all duration-300">
   <img src="../assets/imgs/brand_logo_transparent.png" alt="Portfolio"
-    class="w-5 h-5 rounded-full border border-gray-400">
+    class="w-5 h-5 ">
   <span class="font-medium text-sm">Portfolio</span>
 </a>
   </div>
